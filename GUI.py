e4df78010f78d03b5e6397086a41924438c98804
WillCBarker <100966174+WillCBarker@users.noreply.github.com>
2023-02-14 13:14:06 -0500

    Update GUI.py
    Updated layout, removed white border, added interaction with compounding expenses and compounding investments via sliders in customize div

--- GUI.py
+++ GUI.py
@@ -29,27 +29,20 @@ compoundInterest = x.compoundInterest(futureDay2, 8)
 f1 = px.bar(x = container['expenseNames'], y = container['expenses'])
 f2 = px.line(x = projectedMoney[0], y = projectedMoney[1])
 f3 = px.line(x = compoundInterest[0], y = compoundInterest[1])
-f4 = px.line(x = projectedMoney[0], y = projectedMoney[1])
+f4 = px.line(x = compoundInterest[0], y = compoundInterest[1])
+
 
 f1 = f1.to_dict()
-f1['layout']['paper_bgcolor'] ='#96b8d9'
-f1['layout']['plot_bgcolor'] = '#96b8d9'
 f1['layout']['title'] = 'Expenses'
 
 f2 = f2.to_dict()
-f2['layout']['paper_bgcolor'] ='#96b8d9'
-f2['layout']['plot_bgcolor'] = '#96b8d9'
 f2['layout']['title'] = 'Balances'
 
-f4 = f4.to_dict()
-f4['layout']['paper_bgcolor'] ='#96b8d9'
-f4['layout']['plot_bgcolor'] = '#96b8d9'
-f4['layout']['title'] = 'Taxes'
-
 f3 = f3.to_dict()
-f3['layout']['paper_bgcolor'] ='#96b8d9'
-f3['layout']['plot_bgcolor'] = '#96b8d9'
-f3['layout']['title'] = 'Investments'
+f3['layout']['title'] = 'Expenses Compounded'
+
+f4 = f4.to_dict()
+f4['layout']['title'] = 'Investments'
 
 app = dash.Dash(__name__)
 
@@ -61,101 +54,126 @@ app.layout = html.Div(
             className="Header",
             style={
                 "gridArea": "Header",
-                "backgroundColor": '#96b8d9',
-                "textAlign": "center",
+                "backgroundColor": '#FFFFFF',
                 "height": "80px",
-                "margin-bottom": '350px',
-                'font-size': '40px'
-                #"outline": "black solid 1px"
+                "textAlign": "center",
+                "marginBottom": '5px',
+                "margin" : "0",
+                'fontSize': '40px',
+                "font-family": "Copperplate, sans-serif",
+                "font-weight": "bold"
             },
-            children="This is a financial dashboard",
-        ),
-        html.Div(
-            className="Balances",
-            style={
-                "gridArea": "Balances",
-                #"backgroundColor": "#e5fae3",
-                "display": "flex",
-                "align-items": "center",
-                "justify-content": "center",
-                #"outline": "black solid 1px"
-            },
-            children=[
-        dcc.Graph(id='example-graph1', figure=f2,
-            style = {'height': '400px', 'width': '655px', "textAlign": "center"})
-    ],
-        ),
-        html.Div(
-            className="Taxes",
-            style={
-                "gridArea": "Taxes",
-                #"backgroundColor": "#e5fae3",
-                "display": "flex",
-                "align-items": "center",
-                "justify-content": "center",
-                #"outline": "black solid 1px"
-            },
-            children=[
-        dcc.Graph(id='example-graph2', figure=f4,
-            style = {'height': '400px', 'width': '655px', "textAlign": "center"})
-    ],
-        ),
-        html.Div(
-            className="Investments",
-            style={
-                "gridArea": "Investments",
-                #"backgroundColor": "#e5fae3",
-                "display": "flex",
-                "align-items": "center",
-                "justify-content": "center",
-                #"outline": "black solid 1px"
-            },
-            children=[
-        dcc.Graph(id='example-graph3', figure=f3,
-            style = {'height': '390px', 'width': '655px', "textAlign": "center"})
-    ],
+            children="Budget Bot V0.1",
         ),
         html.Div(
             className="Expenses",
             style={
                 "gridArea": "Expenses",
-                "backgroundColor": '#8dabc9',
                 "display": "flex",
-                "align-items": "center",
-                "justify-content": "center",
-                #"outline": "black solid 1px"
+                "alignItems": "center",
+                "justifyContent": "center",
+                "marginTop": "5px",
             },
             children=[
-        dcc.Graph(id='example-graph4', figure={
-            'data': [{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                     {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'NYC'},
-                     ],
-            'layout': {'paper_bgcolor': '#96b8d9',
-                       'plot_bgcolor': '#96b8d9',
-                       'title': 'Expenses',
-                       },
-        },
-            style = {'height': '390px', 'width': '655px', "textAlign": "center"}
-        )
-    ],
+                    dcc.Graph(id='example-graph1', figure=f1,
+                    style = {'height': '390px', 'width': '655px', "textAlign": "center"})
+                    ],
+        ),
+        html.Div(
+            className="Expenses Compounded",
+            style={
+                "gridArea": "ExpensesCompounded",
+                "display": "flex",
+                "alignItems": "center",
+                "justifyContent": "center",
+                "marginTop": "5px"
+            },
+            children=[
+                    dcc.Graph(id='example-graph2', figure=f3,
+                    style = {'height': '390px', 'width': '655px', "textAlign": "center"})
+                    ]
+            ,
+        ),
+        html.Div(
+            className="Balance Over Time",
+            style={
+                "gridArea": "Balances",
+                "display": "flex",
+                "alignItems": "center",
+                "justifyContent": "center",
+            },
+            children=[
+                    dcc.Graph(id='example-graph3', figure=f2,
+                    style = {'height': '400px', 'width': '655px', "textAlign": "center"})
+                    ],
+        ),
+        html.Div(
+            className="Investments",
+            style={
+                "gridArea": "Investments",
+                "display": "flex",
+                "alignItems": "center",
+                "justifyContent": "center",
+            },
+            children=[
+                    dcc.Graph(id='example-graph4', figure=f4,
+                    style = {'height': '390px', 'width': '655px', "textAlign": "center"})
+                    ],
         ),
         html.Div(
             className="Customize",
             style={
                 "gridArea": "Customize",
-                "backgroundColor": '#96b8d9',
-                #"outline": "black solid 2px",
-                "height": "804px",
-                "margin-top": "-15px"
+                "backgroundColor": '#FFFFFF',
+                "height": "808px",
+                "marginTop": "-15px",
+                "textAlign": "center"
             },
-            children=dcc.Slider(
-                        id='slider',
-                        min=0,
-                        max=100,
-                        step=5,
-                        value=10
-                    ),
-        ),
+            children= [html.Div(
+                        style = {
+                                "marginTop": "20px",
+                                "marginBottom": "20px"
+                        },
+                        children = ["Years of Compounding Balance", 
+                                    dcc.Slider(
+                                        id='slider',
+                                        min=0,
+                                        max=100,
+                                        step=5,
+                                        value=10
+                                        )
+                                    ]
+                                ),
+                        html.Div(
+                        style = {
+                                "marginBottom": "20px"
+                        },
+                        children = ["Years of Compounding Expense", 
+                                    dcc.Slider(
+                                        id='slider1',
+                                        min=0,
+                                        max=100,
+                                        step=5,
+                                        value=10
+                                        )
+                                    ]
+                                ),
+                        html.Div(
+                        style = {
+                                "marginBottom": "20px"
+                        },
+                        children = ["Years of Compounding Investment", 
+                                    dcc.Slider(
+                                        id='slider2',
+                                        min=0,
+                                        max=100,
+                                        step=5,
+                                        value=10
+                                        )
+                                    ]
+                                ),
+                        ]
+                ),
     ],
     style={
         "display": "grid",
@@ -164,11 +182,11 @@ app.layout = html.Div(
         "gap": "60px 20px",
         "padding": "10px",
         "gridAutoFlow": "row",
-        "backgroundColor": '#7594b3 ',
+        "backgroundColor": '#d9ebfc',
         "gridTemplateAreas": '''"Header Header Header"
-                                "Expenses Investments Customize"
-                                "Balances Taxes Customize"''',
-        "height": "96vh"
+                                "Expenses ExpensesCompounded Customize"
+                                "Balances Investments Customize"''',
+        "height": "97.9vh"
     },
 )
 
@@ -176,11 +194,14 @@ app.layout = html.Div(
 current_year = datetime.now()
 
 @app.callback(
-    Output('example-graph1', 'figure'),
-    Input('slider', 'value')
+    Output('example-graph3', 'figure'),
+    Input('slider', 'value'),
 )
 
 def update_Balances(value):
+    '''
+    Updates Balance Over Time chart, scaling money along with input time frame
+    '''
     if value < 1:
         value = 1      
     futureDay = current_year.replace(year=current_year.year + value)
@@ -188,7 +209,44 @@ def update_Balances(value):
     scaled_y = [y for y in projectedMoney[1]]
     scaled_x = [x for x in projectedMoney[0]]
     line = px.line(x=scaled_x, y = scaled_y)
-    line.update_layout(xaxis_title='over {} year(s)'.format(value), yaxis_title="Money")  
+    line.update_layout(xaxis_title='over {} year(s)'.format(value), yaxis_title="Money", title="Balance Over Time")  
+    return line
+
+
+@app.callback(
+    Output('example-graph4', 'figure'),
+    Input('slider2', 'value'),
+)
+
+def update_Investments(value):
+    '''
+    Updates Investments Over Time chart, scaling money along with input time frame
+    '''
+    futureDay = current_year.replace(year=current_year.year + value)
+    compoundedMoney = x.compoundInterest(futureDay, 6)
+    scaled_y = [y for y in compoundedMoney[1]]
+    scaled_x = [x for x in compoundedMoney[0]]
+    line = px.line(x=scaled_x, y = scaled_y)
+    line.update_layout(xaxis_title='over {} year(s)'.format(value), yaxis_title="Money", title="Investments Over Time") 
+    return line
+
+#For update_Expenses_Compounted, use the get total Budget and iterate through, adding each total budget calculation at that respective years x value
+@app.callback(
+    Output('example-graph2', 'figure'),
+    Input('slider1', 'value'),
+)
+
+def update_Compounding_Expenses(value):
+    '''
+    Updates Expenses Over Time chart, scaling money along with input time frame
+    '''
+    scaled_x = []
+    scaled_y = []
+    for year in range(1, value + 1):
+        scaled_y.append((year * x.getTotalBudget()) * 12)
+        scaled_x.append(year)
+    line = px.line(x=scaled_x, y = scaled_y)
+    line.update_layout(xaxis_title='over {} year(s)'.format(value), yaxis_title="Money", title="Expenses Compounded Over Time") 
     return line
 
 if __name__ == "__main__":
